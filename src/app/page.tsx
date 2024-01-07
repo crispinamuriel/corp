2719aaf6c2aa68eaef2e1fad870037b35577deba
crispinamuriel <crispinamuriel@gmail.com>
2024-01-07 15:41:00 -0500

    create reusable hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import Image from 'next/image';
 import homeImg from 'public/home.jpg';
+import Hero from '@/components/hero';
 
 export default function Home() {
-  return (<div>
-    <div className="flex flex-col justify-center max-w-500px items-center h-screen">
-      <h1 className="text-4xl font-bold text-white">Professional Cloud Hosting</h1>
-      <div className="absolute -z-10 inset-0">
-        <Image
-          alt="car assembly line"
-          src={homeImg}
-          fill
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
-    </div>
-  </div>
-
+  return (
+    <Hero imgData={homeImg} imgAlt={"assembly line"} title={"Professional Cloud Hosting"} />
   )
 }
--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import Hero from '@/components/hero';
+import reliabilityImg from 'public/reliability.jpg';
 
 export default function Reliability() {
   return (
-    <div className="flex flex-col justify-center max-w-500px items-center h-screen">
-      <h1 className="text-4xl font-bold">Reliability is our number one priority</h1>
-      <img alt="staircase image" src="https://images.unsplash.com/photo-1466278313810-9aca9abea861?q=80&w=3397&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="500px"></img>
-
-    </div>
+    <Hero imgData={reliabilityImg} imgAlt={"assembly line"} title={"Reliability is our number one priority"} />
   )
 }
